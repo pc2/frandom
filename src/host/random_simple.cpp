1103448fbec55f03c3901db5c7ab22e683d5f097
Marius Meyer <marius.meyer@upb.de>
2019-08-05 16:52:59 +0200

    Implement emb. parallel mode for simple host

--- src/host/random_simple.cpp
+++ src/host/random_simple.cpp
@@ -60,7 +60,7 @@ extern void calculate(cl::Context context, cl::Device device, cl::Program progra
 extern void print_results();
 
 
-static double timearray[NTIMES + 1];
+static double timearray[2][NTIMES + 1];
 
 int main(int argc, char * argv[])
 {
@@ -71,33 +71,33 @@ int main(int argc, char * argv[])
     int err;
 // Setting up OpenCL for FPGA
     //Setup Platform
-	//Get Platform ID
-	std::vector<cl::Platform> PlatformList;
-	err = cl::Platform::get(&PlatformList);
-	assert(err==CL_SUCCESS);
+    //Get Platform ID
+    std::vector<cl::Platform> PlatformList;
+    err = cl::Platform::get(&PlatformList);
+    assert(err==CL_SUCCESS);
 
     cl::Platform platform = PlatformList[0];
     std::cout << "Platform Name: " << platform.getInfo<CL_PLATFORM_NAME>() << std::endl;
 
-	//Setup Device
-	//Get Device ID
-	std::vector<cl::Device> DeviceList;
-	err = PlatformList[0].getDevices(CL_DEVICE_TYPE_ACCELERATOR, &DeviceList);
-	assert(err==CL_SUCCESS);
-	
-	//Create Context
-	cl::Context context(DeviceList);
-	assert(err==CL_SUCCESS);
+    //Setup Device
+    //Get Device ID
+    std::vector<cl::Device> DeviceList;
+    err = PlatformList[0].getDevices(CL_DEVICE_TYPE_ACCELERATOR, &DeviceList);
+    assert(err==CL_SUCCESS);
+
+    //Create Context
+    cl::Context context(DeviceList);
+    assert(err==CL_SUCCESS);
     std::cout << "Device Name:   " << DeviceList[0].getInfo<CL_DEVICE_NAME>() << std::endl;
 
     if  ( (quantum = checktick()) >= 1) {
-        std::cout << "Your clock granularity/precision appears to be " << quantum 
+        std::cout << "Your clock granularity/precision appears to be " << quantum
                 << " microseconds." << std::endl;
     }
     else {
         std::cout << "Your clock granularity appears to be less than one microsecond."
                 << std::endl;
-	    quantum = 1;
+        quantum = 1;
     }
 
     std::cout << HLINE;
@@ -107,17 +107,16 @@ int main(int argc, char * argv[])
 
     DeviceList.resize(1);
 
-	//Read in binaries from file
+    //Read in binaries from file
     char* used_kernel = (char*) FPGA_KERNEL_FILE_GLOBAL;
     if (argc > 1) {
         std::cout << "Using kernel given as argument" << std::endl;
         used_kernel = argv[1];
     }
     fpga_setup_and_execution(context, DeviceList, used_kernel);
-    
+
 
     // Print results
-    std::cout << "       best       mean      GUOPS      error" << std::endl;
     print_results();
 
     return 0;
@@ -130,19 +129,19 @@ void fpga_setup_and_execution(cl::Context context, std::vector<cl::Device> Devic
     if (!aocx_stream.is_open()){
         std::cerr << "Not possible to open from given file!" << std::endl;
     }
-    
-	std::string prog(std::istreambuf_iterator<char>(aocx_stream), (std::istreambuf_iterator<char>()));
+
+    std::string prog(std::istreambuf_iterator<char>(aocx_stream), (std::istreambuf_iterator<char>()));
     aocx_stream.seekg(0, aocx_stream.end);
     unsigned file_size = aocx_stream.tellg();
     aocx_stream.seekg(0,aocx_stream.beg);
     char * buf = new char[file_size];
     aocx_stream.read(buf, file_size);
 
-	cl::Program::Binaries mybinaries;
+    cl::Program::Binaries mybinaries;
     mybinaries.push_back({buf, file_size});
 
-	// Create the Program from the AOCX file.
-	cl::Program program(context, DeviceList, mybinaries);
+    // Create the Program from the AOCX file.
+    cl::Program program(context, DeviceList, mybinaries);
 
     std::cout << "Prepared FPGA successfully for global Execution!" << std::endl;
     std::cout << HLINE;
@@ -152,40 +151,61 @@ void fpga_setup_and_execution(cl::Context context, std::vector<cl::Device> Devic
 }
 
 void print_results() {
+    std::cout << std::setw(11) << "type" << std::setw(11) << "best" << std::setw(11)
+              << "mean" << std::setw(11) << "GUOPS"
+              << std::setw(11) << "error" / DATA_LENGTH << std::endl;
+
+    // Calculate performance for kernel runtime only
     double gups = (4 * DATA_LENGTH) / 1000000000;
     double tmean = 0;
     double tmin = DBL_MAX;
     //calculate mean
     for (int i=0; i<NTIMES;i++) {
-        tmean += timearray[i];
-        if (timearray[i] < tmin) {
-            tmin = timearray[i];
+        tmean += timearray[1][i];
+        if (timearray[1][i] < tmin) {
+            tmin = timearray[1][i];
         }
     }
     tmean = tmean / NTIMES;
 
     double error_count = timearray[NTIMES];
+    std::cout << std::setw(11) << "calc" << std::setw(11) << tmin << std::setw(11)
+              << tmean << std::setw(11) << gups / tmin
+              << std::setw(11) << (100.0 * error_count) / DATA_LENGTH << std::endl;
 
-    std::cout << std::setw(11) << tmin << std::setw(11) << tmean << std::setw(11) << gups / tmin
+    // Calculate performance for kernel execution plus data transfer
+    tmean = 0;
+    tmin = DBL_MAX;
+    for (int i=0; i<NTIMES;i++) {
+        double tmp_total = timearray[0][i] timearray[1][i];
+        tmean +=  tmp_total;
+        if (tmp_total < tmin) {
+            tmin = tmp_total;
+        }
+    }
+    tmean = tmean / NTIMES;
+
+    std::cout << std::setw(11) << "calc+comm" << std::setw(11) << tmin << std::setw(11)
+              << tmean << std::setw(11) << gups / tmin
               << std::setw(11) << (100.0 * error_count) / DATA_LENGTH << std::endl;
 }
 
-# define	M	20
+# define    M    20
 
 int
 checktick()
     {
-    int		i, minDelta, Delta;
-    double	t1, t2, timesfound[M];
+    int        i, minDelta, Delta;
+    double    t1, t2, timesfound[M];
 
 /*  Collect a sequence of M unique time values from the system. */
 
     for (i = 0; i < M; i++) {
-	t1 = mysecond();
-	while( ((t2=mysecond()) - t1) < 1.0E-6 )
-	    ;
-	timesfound[i] = t1 = t2;
-	}
+    t1 = mysecond();
+    while( ((t2=mysecond()) - t1) < 1.0E-6 )
+        ;
+    timesfound[i] = t1 = t2;
+    }
 
 /*
  * Determine the minimum difference between these M values.
@@ -195,9 +215,9 @@ checktick()
 
     minDelta = 1000000;
     for (i = 1; i < M; i++) {
-	Delta = (int)( 1.0E6 * (timesfound[i]-timesfound[i-1]));
-	minDelta = MIN(minDelta, MAX(Delta,0));
-	}
+    Delta = (int)( 1.0E6 * (timesfound[i]-timesfound[i-1]));
+    minDelta = MIN(minDelta, MAX(Delta,0));
+    }
 
    return(minDelta);
     }
@@ -220,108 +240,114 @@ double mysecond()
 }
 
 void calculate(cl::Context context, cl::Device device, cl::Program program){
-	DATA_TYPE_UNSIGNED* random;
-	posix_memalign((void **)&random,64, sizeof(DATA_TYPE_UNSIGNED)*RANDOM_LENGTH);
+    DATA_TYPE_UNSIGNED* random;
+    posix_memalign((void **)&random,64, sizeof(DATA_TYPE_UNSIGNED)*RANDOM_LENGTH);
     int err;
 
-	for (DATA_TYPE i=0; i<RANDOM_LENGTH; i++) {
-		random[i] = starts((4 * DATA_LENGTH) / RANDOM_LENGTH * i);
-	}
-	std::cout << "Prepare FPGA instances" << std::endl;
+    for (DATA_TYPE i=0; i<RANDOM_LENGTH; i++) {
+        random[i] = starts((4 * DATA_LENGTH) / RANDOM_LENGTH * i);
+    }
+    std::cout << "Prepare FPGA instances" << std::endl;
 
-	std::vector<cl::CommandQueue> compute_queue;
-	std::vector<cl::Buffer> Buffer_data;
-	std::vector<cl::Buffer> Buffer_random;
-	std::vector<cl::Kernel> accesskernel;
-	std::vector<DATA_TYPE*> data_sets;
+    std::vector<cl::CommandQueue> compute_queue;
+    std::vector<cl::Buffer> Buffer_data;
+    std::vector<cl::Buffer> Buffer_random;
+    std::vector<cl::Kernel> accesskernel;
+    std::vector<DATA_TYPE*> data_sets;
 
-	for (int r=0; r < REPLICATIONS; r++) {
-		DATA_TYPE* data;
-    	posix_memalign((void **)&data,64, sizeof(DATA_TYPE)*(DATA_LENGTH / REPLICATIONS));
-		data_sets.push_back(data);
-		// Prepare kernel with ID $rep$
-		compute_queue.push_back(cl::CommandQueue(context, device)); 
-		Buffer_data.push_back(cl::Buffer(context, CL_CHANNEL_1_INTELFPGA, sizeof(DATA_TYPE)*(DATA_LENGTH / REPLICATIONS)));
+    for (int r=0; r < REPLICATIONS; r++) {
+        DATA_TYPE* data;
+        posix_memalign((void **)&data,64, sizeof(DATA_TYPE)*(DATA_LENGTH / REPLICATIONS));
+        data_sets.push_back(data);
+        // Prepare kernel with ID $rep$
+        compute_queue.push_back(cl::CommandQueue(context, device));
+        Buffer_data.push_back(cl::Buffer(context, CL_CHANNEL_1_INTELFPGA, sizeof(DATA_TYPE)*(DATA_LENGTH / REPLICATIONS)));
 
-		// manually calculate channel flag specified in cl_ext_intelfpga.h as CL_CHANNEL_N_INTELFPGA
-		// TODO: change this in case the preprocessor define changes
-		int channel = (r % 8) << 16;
-		Buffer_random.push_back(cl::Buffer(context, CL_MEM_WRITE_ONLY| channel , sizeof(DATA_TYPE_UNSIGNED) * RANDOM_LENGTH));
+        // manually calculate channel flag specified in cl_ext_intelfpga.h as CL_CHANNEL_N_INTELFPGA
+        // TODO: change this in case the preprocessor define changes
+        int channel = (r % 8) << 16;
+        Buffer_random.push_back(cl::Buffer(context, CL_MEM_WRITE_ONLY| channel , sizeof(DATA_TYPE_UNSIGNED) * RANDOM_LENGTH));
 
-		accesskernel.push_back(cl::Kernel(program, ("accessMemory" + std::to_string(r)).c_str() , &err));
-		assert(err==CL_SUCCESS);
-		//prepare kernels
-		err = accesskernel[r].setArg(0, Buffer_data[r]);
-		assert(err==CL_SUCCESS);
-		err = accesskernel[r].setArg(1, DATA_TYPE_UNSIGNED(DATA_LENGTH));
-		assert(err==CL_SUCCESS);
-		err = accesskernel[r].setArg(2, DATA_TYPE_UNSIGNED(DATA_LENGTH / REPLICATIONS));
-		assert(err==CL_SUCCESS);
-		err = accesskernel[r].setArg(3, Buffer_random[r]);
-		assert(err==CL_SUCCESS);		
-	}
+        accesskernel.push_back(cl::Kernel(program, ("accessMemory" + std::to_string(r)).c_str() , &err));
+        assert(err==CL_SUCCESS);
+        //prepare kernels
+        err = accesskernel[r].setArg(0, Buffer_data[r]);
+        assert(err==CL_SUCCESS);
+        err = accesskernel[r].setArg(1, DATA_TYPE_UNSIGNED(DATA_LENGTH));
+        assert(err==CL_SUCCESS);
+        err = accesskernel[r].setArg(2, DATA_TYPE_UNSIGNED(DATA_LENGTH / REPLICATIONS));
+        assert(err==CL_SUCCESS);
+        err = accesskernel[r].setArg(3, Buffer_random[r]);
+        assert(err==CL_SUCCESS);
+    }
 
     double t;
     double tmin = DBL_MAX;
-	std::cout << "Start actual execution" << std::endl;
-    t = mysecond();
     for (int i = 0; i < NTIMES; i++){
-		for (DATA_TYPE r =0; r < REPLICATIONS; r++) {
-			std::cout << "row" << r << std::endl;
-			for (DATA_TYPE j=0; j<(DATA_LENGTH / REPLICATIONS); j++) {
-				data_sets[r][j] = r*(DATA_LENGTH / REPLICATIONS) + j;
-			}
-		}
-		std::cout << "Prepared data for the next run" << std::endl;
-		for (int r=0; r < REPLICATIONS; r++) {
-			compute_queue[r].enqueueWriteBuffer(Buffer_random[r], CL_TRUE, 0, sizeof(DATA_TYPE_UNSIGNED) * RANDOM_LENGTH, random);
-			compute_queue[r].enqueueWriteBuffer(Buffer_data[r], CL_TRUE, 0, sizeof(DATA_TYPE)*(DATA_LENGTH / REPLICATIONS), data_sets[r]);
-		}
+        std::cout << "Start run " << i << std::endl;
         t = mysecond();
-		for (int r=0; r < REPLICATIONS; r++) {
-        	compute_queue[r].enqueueTask(accesskernel[r]);
-		}
-		for (int r=0; r < REPLICATIONS; r++) {
-			compute_queue[r].finish();
-		}
-		t = mysecond() - t;
-		timearray[i] = t;
-		if (t < tmin) {
-			tmin = t;
-		}
+        for (DATA_TYPE i=0; i<RANDOM_LENGTH; i++) {
+            random[i] = starts((4 * DATA_LENGTH) / RANDOM_LENGTH * i);
+        }
+        for (DATA_TYPE r =0; r < REPLICATIONS; r++) {
+            for (DATA_TYPE j=0; j<(DATA_LENGTH / REPLICATIONS); j++) {
+                #ifndef EMBARRASSINGLY_PARALLEL
+                data_sets[r][j] = r*(DATA_LENGTH / REPLICATIONS) + j;
+                #else
+                data_sets[r][j] = r*(DATA_LENGTH / REPLICATIONS) + j;
+                #endif
+            }
+        }
+        for (int r=0; r < REPLICATIONS; r++) {
+            compute_queue[r].enqueueWriteBuffer(Buffer_random[r], CL_TRUE, 0, sizeof(DATA_TYPE_UNSIGNED) * RANDOM_LENGTH, random);
+            compute_queue[r].enqueueWriteBuffer(Buffer_data[r], CL_TRUE, 0, sizeof(DATA_TYPE)*(DATA_LENGTH / REPLICATIONS), data_sets[r]);
+        }
+        timearray[0][i] = mysecond() - t;
+        t = mysecond();
+        for (int r=0; r < REPLICATIONS; r++) {
+            compute_queue[r].enqueueTask(accesskernel[r]);
+        }
+        for (int r=0; r < REPLICATIONS; r++) {
+            compute_queue[r].finish();
+        }
+        t = mysecond() - t;
+        timearray[1][i] = t;
+        if (t < tmin) {
+            tmin = t;
+        }
     }
-	std::cout << "Read back results" << std::endl;
+    std::cout << "Read back results" << std::endl;
 
-	for (int r=0; r < REPLICATIONS; r++) {
-		compute_queue[r].enqueueReadBuffer(Buffer_data[r], CL_TRUE, 0, sizeof(DATA_TYPE)*(DATA_LENGTH / REPLICATIONS), data_sets[r]);
-	} 
-	DATA_TYPE* data;
+    for (int r=0; r < REPLICATIONS; r++) {
+        compute_queue[r].enqueueReadBuffer(Buffer_data[r], CL_TRUE, 0, sizeof(DATA_TYPE)*(DATA_LENGTH / REPLICATIONS), data_sets[r]);
+    }
+    DATA_TYPE* data;
     posix_memalign((void **)&data,64, (sizeof(DATA_TYPE)*DATA_LENGTH));
-	for (DATA_TYPE j=0; j<(DATA_LENGTH / REPLICATIONS); j++) {
-		for (DATA_TYPE r =0; r < REPLICATIONS; r++) {
-			data[r*(DATA_LENGTH / REPLICATIONS) + j] = data_sets[r][j];
-		}
-	}
+    for (DATA_TYPE j=0; j<(DATA_LENGTH / REPLICATIONS); j++) {
+        for (DATA_TYPE r =0; r < REPLICATIONS; r++) {
+            data[r*(DATA_LENGTH / REPLICATIONS) + j] = data_sets[r][j];
+        }
+    }
 
-	/* --- Check Results --- */
-	DATA_TYPE_UNSIGNED temp = 1;
-	for (int i =0;i<4*DATA_LENGTH; i++) {
-		DATA_TYPE_UNSIGNED v = 0;
-		if (((DATA_TYPE) temp) < 0) {
-			v = POLY;
-		}
-		temp = (temp << 1) ^ v;
-		data[temp & (DATA_LENGTH - 1)] ^= temp;
-	}
+    /* --- Check Results --- */
+    DATA_TYPE_UNSIGNED temp = 1;
+    for (int i =0;i<4*DATA_LENGTH; i++) {
+        DATA_TYPE_UNSIGNED v = 0;
+        if (((DATA_TYPE) temp) < 0) {
+            v = POLY;
+        }
+        temp = (temp << 1) ^ v;
+        data[temp & (DATA_LENGTH - 1)] ^= temp;
+    }
 
-	double correct = 0;
-	for (int i=0; i< DATA_LENGTH; i++) {
-		if (data[i] != i) {
-			correct++;
-		}
-	}
+    double correct = 0;
+    for (int i=0; i< DATA_LENGTH; i++) {
+        if (data[i] != i) {
+            correct++;
+        }
+    }
 
-    timearray[NTIMES] = correct;
+    timearray[0][NTIMES] = correct;
     free((void*)data);
 }
 
@@ -330,52 +356,52 @@ void calculate(cl::Context context, cl::Device device, cl::Program program){
 
  */
 DATA_TYPE_UNSIGNED starts(DATA_TYPE n) {
-	DATA_TYPE_UNSIGNED m2[BIT_SIZE];
+    DATA_TYPE_UNSIGNED m2[BIT_SIZE];
 
-	while (n < 0) {
-		n += PERIOD;
-	}
-	while (n > 0) {
-		n -= PERIOD;
-	}
+    while (n < 0) {
+        n += PERIOD;
+    }
+    while (n > 0) {
+        n -= PERIOD;
+    }
 
-	if (n == 0) {
-		return 1;
-	}
+    if (n == 0) {
+        return 1;
+    }
 
-	DATA_TYPE_UNSIGNED temp = 1;
-	for (int i=0;i<BIT_SIZE;i++) {
-		m2[i] = temp;
-		for (int j=0; j<2;j++) {
-			DATA_TYPE_UNSIGNED v = 0;
-			if (((DATA_TYPE) temp) < 0) {
-				v = POLY;
-			}
-			temp = (temp << 1) ^ v;
-		}
-	}
-	DATA_TYPE i = BIT_SIZE - 2;
-	while (i >= 0 && !((n >> i) & 1)) {
-		i--;
-	}
+    DATA_TYPE_UNSIGNED temp = 1;
+    for (int i=0;i<BIT_SIZE;i++) {
+        m2[i] = temp;
+        for (int j=0; j<2;j++) {
+            DATA_TYPE_UNSIGNED v = 0;
+            if (((DATA_TYPE) temp) < 0) {
+                v = POLY;
+            }
+            temp = (temp << 1) ^ v;
+        }
+    }
+    DATA_TYPE i = BIT_SIZE - 2;
+    while (i >= 0 && !((n >> i) & 1)) {
+        i--;
+    }
 
-	DATA_TYPE_UNSIGNED ran = 2;
-	while (i > 0) {
-		temp = 0;
-		for (int j=0; j < BIT_SIZE; j++) {
-			if ((ran >> j) & 1) {
-				temp ^= m2[j];
-			}
-		}
-		ran = temp;
-		i--;
-		if ((n>>i) & 1) {
-			DATA_TYPE_UNSIGNED v = 0;
-			if (((DATA_TYPE) ran) < 0) {
-				v = POLY;
-			}
-			ran = (ran << 1) ^v;
-		}
-	}
-	return ran;
+    DATA_TYPE_UNSIGNED ran = 2;
+    while (i > 0) {
+        temp = 0;
+        for (int j=0; j < BIT_SIZE; j++) {
+            if ((ran >> j) & 1) {
+                temp ^= m2[j];
+            }
+        }
+        ran = temp;
+        i--;
+        if ((n>>i) & 1) {
+            DATA_TYPE_UNSIGNED v = 0;
+            if (((DATA_TYPE) ran) < 0) {
+                v = POLY;
+            }
+            ran = (ran << 1) ^v;
+        }
+    }
+    return ran;
 }
